7c4bbaf20c99312421223ffce36914738a98ed03
Aleksandr Afanasiev <afanasiev.aleksandr.92@gmail.com>
2020-05-20 12:16:46 +0300

    Initial Commit

---
+++ circlecitemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  circlecitemplate
+//
+//  Created by Aleksandr Afanasiev on 20.05.2020.
+//  Copyright © 2020 Aleksandr Afanasiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
